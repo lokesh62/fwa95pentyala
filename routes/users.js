a46c6b084c7c9e339a2a4c9c87cad12942b877e2
lokesh62 <lokeshpentyala1999@gmail.com>
2021-10-27 19:36:49 -0500

    added

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var val = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  val=val+1;
   res.send('respond with a resource');
 });
 
