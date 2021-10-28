73bb41618d8b211a03cd3f63ee0704af7d0307fc
lokesh62 <lokeshpentyala1999@gmail.com>
2021-10-27 19:44:43 -0500

    added

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var val = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   val=val+1;
-  res.send('respond with a resource');
+  res.send(`user accesses are: ${val}`);
 });
 
 module.exports = router;
